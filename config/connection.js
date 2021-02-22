2eb625a31b3b0ce18c44d81e47f9002d81a8d69b
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-02-22 15:48:32 +0800

    add server.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8080,
+        user: "root",
+        password: 'Y3in@mysql',
+        database: "burger_db"
+    });
+}
+
+connection.connect();
+
+module.exports = connection;
